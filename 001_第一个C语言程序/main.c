2a46a5aeb621f18e2d84301078c29a6d299c608a
lingwh <LRwq942133>
2023-01-07 16:33:37 +0800

    初始化工程

---
+++ 001_第一个C语言程序/main.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+/**
+ * 章节内容:
+ *  1.C语言开发环境搭建
+ *  2.C语言参考手册
+ *  3.第一个C语言程序
+ *
+ * MinGw
+ *      https://sourceforge.net/projects/mingw-w64/
+ * C语言参考网站
+ *      https://zh.cppreference.com/w/
+ *      https://cplusplus.com/
+ * @return
+ */
+int main() {
+    printf("Hello world!");
+    return 0;
+}
