1f9af42bb357d426a1dbe8715d818ef5fb8a7538
think <lingwh.walkman@hotmail.com>
2025-04-10 15:38:11 +0800

    修改项目结构

--- 001_第一个C语言程序/main.c
+++ 001_第一个C语言程序/main.c
@@ -37,17 +37,20 @@
  */
 
 #if 0
+#endif
 int main() {
 	system("calc");
+	printf("hello world~\n");
+	printf("你好世界~\n");
 	system("pause");
 	return 0;
 }
-#endif
+
 
 #if 0
-#endif
 int main() {
 	printf("Hello world!");
 	return 0;
 }
+#endif
 
