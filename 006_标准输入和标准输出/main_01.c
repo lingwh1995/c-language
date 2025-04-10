9cf6929abfab7b1c9fb9962976294a0e08921277
lingwh <lingwh.walkman@homail.com>
2025-04-10 22:05:47 +0800

    修改项目结构

--- 006_标准输入和标准输出/main_01.c
+++ 006_标准输入和标准输出/main_01.c
@@ -5,15 +5,13 @@
  */
 
 
-/**
-* 标准输入测试
-*/
-void StdinAndEOFTest1()
-{
+ /**
+ * 标准输入测试
+ */
+void StdinAndEOFTest1() {
     printf("请输入,按回车结束:\n");
     int sum = 0;
-    while (getchar() != '\n')
-    {
+    while (getchar() != '\n') {
         sum++;
     }
     printf("sum = %d\n", sum);
@@ -23,12 +21,10 @@ void StdinAndEOFTest1()
 /**
  * 标准输入和EOF测试
  */
-void StdinAndEOFTest2()
-{
+void StdinAndEOFTest2() {
     printf("请输入,按 换行符+Ctrl+D 结束:\n");
     int sum = 0;
-    while (getchar() != EOF)
-    {
+    while (getchar() != EOF) {
         sum++;
     }
     printf("sum = %d\n", sum);
@@ -39,8 +35,7 @@ void StdinAndEOFTest2()
  * 标准输入测试
  *  接收单个字符: 不管输入多少个字符,都只会接收第一个字符
  */
-void StdinAndEOFTest3()
-{
+void StdinAndEOFTest3() {
     printf("请输入:\n");
     char ch = getchar();
     printf("ch = %c", ch);
@@ -51,8 +46,7 @@ void StdinAndEOFTest3()
  * 标准输入测试
  *  接收多个字符
  */
-void StdinAndEOFTest4()
-{
+void StdinAndEOFTest4() {
     printf("请输入:\n");
     char ch = '\0';
     //为什么getchar()可以放在while后面的大括号中,来接收多个字符,因为getchar()有一个缓冲区机制
@@ -61,14 +55,12 @@ void StdinAndEOFTest4()
     //while (ch = getchar() != '\n') {
     //正确写法
     //while ((ch = getchar()) != '\n') {
-    while ((ch = getchar()) != EOF)
-    {
+    while ((ch = getchar()) != EOF) {
         printf("ch = %c\n", ch);
     }
 }
 
-int main()
-{
+int main() {
     //StdinAndEOFTest1();
     //StdinAndEOFTest2();
     //StdinAndEOFTest3();
